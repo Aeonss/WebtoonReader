4a53b4b26d4736f8f7a2ca59d2769f5c5be8a1fa
Aeonss <30298077+Aeonss@users.noreply.github.com>
2022-09-09 17:24:28 -0400

    Initial Commit

---
+++ CustomScroller.py
@@ -0,0 +1,112 @@
+# Referenced from: https://stackoverflow.com/a/56046307
+
+import tkinter as tk
+import os
+from PIL import ImageTk, Image
+
+# Custom infinite seamless vertical image scroller using Tkinter Canvas and Scrollbar
+class ImageScroller(tk.Frame):
+    def __init__(self, master=None, **kw):
+        
+        # Initialization
+        self.path = kw.pop('path', None)
+        self.width = kw.pop('width', None)
+        self.height = kw.pop('height', None)
+        self.scroll_speed = kw.pop('speed', None)
+        sw = kw.pop('scrollbarwidth', 10)
+        super(ImageScroller, self).__init__(master=master, **kw)
+        self.canvas = tk.Canvas(self, width=self.width, height=self.height, highlightthickness=0, **kw)
+        
+        
+        # List of chapter images
+        self.images = []
+        
+        # Fill the frame with images
+        if self.path != "":
+            self.fill()
+        
+        # Create vertical scrollbar
+        self.v_scroll = tk.Scrollbar(self, orient='vertical', width=sw)
+
+        # Grid and configure weight
+        self.canvas.grid(row=0, column=0,  sticky='nsew')
+        self.v_scroll.grid(row=0, column=1, sticky='ns')
+        self.rowconfigure(0, weight=1)
+        self.columnconfigure(0, weight=1)
+        
+        # Set the scrollbars to the canvas
+        self.canvas.config(yscrollcommand=self.v_scroll.set)
+        
+        # Set canvas view to the scrollbars
+        self.v_scroll.config(command=self.canvas.yview)
+
+        # Scroll to go through canvas
+        self.canvas.config(scrollregion=self.canvas.bbox('all'))
+        self.canvas.bind_class(self.canvas, "<MouseWheel>", self.mouse_scroll)
+        self.canvas.bind("<Button-4>", self.mouse_scroll)
+        self.canvas.bind("<Button-5>", self.mouse_scroll)
+        
+        # Click to drag through canvas
+        self.canvas.bind("<Button-1>", self.start_scroll)
+        self.canvas.bind("<B1-Motion>", self.update_scroll)
+        self.canvas.bind("<ButtonRelease-1>", self.stop_scroll)
+
+    # Mouse scroll handling
+    def mouse_scroll(self, event):
+        # Linux uses event.num
+        if event.num == 4 or event.delta > 0:
+            self.canvas.yview_scroll(self.scroll_speed * -1, "units" )
+            
+        # Windows / Mac
+        elif event.num == 5 or event.delta < 0:
+            self.canvas.yview_scroll(self.scroll_speed, "units" )
+            
+            #if self.v_scroll.get()[1] == 1.0
+            
+            
+    # Mouse drag handling
+    # https://shortrecipes.blogspot.com/2014/05/python-3-and-tkinter-scroll-canvas-with.html
+    def start_scroll(self, event):
+        self.canvas.config(yscrollincrement=3) 
+        self.canvas.config(xscrollincrement=3) 
+        self._starting_drag_position = (event.x, event.y)
+        
+        # https://anzeljg.github.io/rin2/book2/2405/docs/tkinter/cursors.html
+        self.canvas.config(cursor="hand2")
+    
+        
+    def update_scroll(self, event):  
+        deltaX = event.x - self._starting_drag_position[0]
+        deltaY = event.y - self._starting_drag_position[1]
+        self.canvas.xview('scroll', deltaX, 'units')
+        self.canvas.yview('scroll', deltaY, 'units')
+        self._starting_drag_position =  (event.x, event.y)
+           
+        
+    def stop_scroll(self, event):
+        self.canvas.config(xscrollincrement=0) 
+        self.canvas.config(yscrollincrement=0)
+        self.canvas.config(cursor="")
+        
+        
+        
+    # Fills the frame with images from the folder path
+    def fill(self):
+        # Gets all images from directory and adds to list
+        for name in os.listdir(self.path):
+            img = Image.open(os.path.join(self.path, name))
+            
+            # Rescales all images
+            if img.width != self.width:
+                scale = img.height / img.width
+                img = img.resize((self.width, int(self.width * scale)), Image.Resampling.LANCZOS)
+
+            # Adds to list to prevent garbage collection
+            self.images.append(ImageTk.PhotoImage(img))
+        
+        height = 0
+        # Creates each image and updates the height to place next image
+        for i in range(len(self.images)):
+            self.canvas.create_image(0, height, anchor=tk.NW, image=self.images[i])
+            height = height + self.images[i].height()
+    
---
+++ README.md
@@ -0,0 +1,82 @@
+<p align="center"><img src="https://i.imgur.com/LTBllUG.png" width="230" height="160"/></p>
+
+<h1 align="center">WebtoonReader</h1>
+
+<font size="3"><a href="https://github.com/Aeonss/WebtoonReader/releases/latest/">WebtoonReader</a> is free and open-source manga/webtoon reader for locally downloaded mangas and webtoons. It is **NOT** a webapp nor does it require a server setup.</font>
+
+<br>
+<p align="center">
+<a href="https://github.com/Aeonss/WebtoonReader/releases/latest/"><img src="https://img.shields.io/github/v/release/Aeonss/WebtoonReader?style=for-the-badge&label=%20%F0%9F%93%A3%20Latest%20release&color=778beb&labelColor=2f3542"/></a>
+<img src="https://img.shields.io/github/stars/Aeonss/WebtoonReader?style=for-the-badge&label=%E2%AD%90%20Stars&color=786fa6&labelColor=2f3542"/>
+<img src="https://img.shields.io/github/downloads/Aeonss/WebtoonReader/total.svg?style=for-the-badge&label=%E2%AC%87%EF%B8%8FDownloads&color=4b6584&labelColor=2f3542"/>
+</p>
+
+<br>
+<p align="center"><img src="https://i.imgur.com/h5Mh2mz.png" width="735" height="850"/></p>
+
+
+<font size="3">
+
+## 🚀 &nbsp; Features
+* Comfortably read locally stored manga/webtoon
+* Automatically bookmarks each manga/webtoon
+* Configure manga/webtoon width, height, and scroll speed
+* Clean and intuitive interface
+
+## 📝 &nbsp; How To Use
+
+<h2>Library Setup</h2>
+
+```
+Library/
+├── Manga 1/
+│   ├── Chapter 1/
+│   │   ├── 0.jpg
+│   │   ├── 1.jpg
+│   │   └── ..
+│   └── Chapter 2/
+│       ├── page-0.png
+│       ├── page-1.png
+│       └── ..
+└── Manga 2/
+    ├── c-4/
+    │   ├── image-1.jpg
+    │   ├── image-2.jpg
+    │   └── ...
+    └── c-5/
+        ├── 0001.jpg
+        └── 0002.jpg
+```
+
+**IMPORTANT**: Folders names do not matter, but ***chapters names and images must be named alphanumerically***.
+
+To download manga/webtoon chapters, use [HakuNeko](https://hakuneko.download/).
+
+
+<h2>Intial Download</h2>
+<ol>
+<li>Load the library by clicking on the "Settings" button at the top and then "Load Library"</li>
+<li>Load a chapter by clicking on the "Load Chapter" button at the top</li>
+<li>To load the next chapter, press "d" or the right arrow key</li>
+</ol>
+
+WebtoonReader will bookmark where you left off on each Manga after each session.
+
+
+
+## ✅ &nbsp; Additional Information
+* WebtoonReader was created with **Python3** and **Tkinter**
+* "Load Manga" will load the chapter where you last left off on the manga
+* "Load Chapter" will load a specific chapter
+* Use "a" or the left arrow key to go to the previous chapter
+* Use "d" or the right arrow key to go to the next chapter
+* Use the mouse scroll wheel, [mouse button 4/5](https://qph.cf2.quoracdn.net/main-qimg-830f83b6de3be2c19c961a05b6e50d57), or mouse drag on the manga to read
+* Please request any features or report any bugs in [issues](https://github.com/Aeonss/WebtoonReader/issues).
+
+## 🤖 &nbsp; To Do
+* Squash all the bugs!
+
+## 📘 &nbsp; License
+WebtoonReader is released under the [MIT license](https://github.com/Aeonss/WebtoonReader/blob/master/LICENSE.md).
+
+</font>
